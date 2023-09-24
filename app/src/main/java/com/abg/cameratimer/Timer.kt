d6719ecd6ea00fdeff1c88d06996cc67ac65ff1f
vadim <gabderahmanov99@gmail.com>
2023-09-24 13:27:44 +0300

    timer

---
+++ app/src/main/java/com/abg/cameratimer/Timer.kt
@@ -0,0 +1,59 @@
+package com.abg.cameratimer
+
+import android.os.CountDownTimer
+
+class Timer(private val time: Long) {
+    private var handler: TimerHandler? = null
+    private var timer: CountDownTimer? = null
+
+    var timeStartFrom = time
+        private set
+
+    private val interval = 1000L
+
+    var isStart = false
+        private set
+
+    fun setTimerHandler(handler: TimerHandler) {
+        this.handler = handler
+    }
+
+    fun startTimer() {
+        if (timeStartFrom > 1_000) {
+            isStart = true
+            timer = object : CountDownTimer(timeStartFrom, interval) {
+                override fun onTick(millisUntilFinished: Long) {
+                    timeStartFrom = millisUntilFinished
+                    handler?.showTime(millisUntilFinished)
+                }
+
+                override fun onFinish() {
+                    isStart = false
+                    handler?.finishTime()
+                }
+            }.start()
+        }
+    }
+
+    fun restart(handler: TimerHandler) {
+        setTimerHandler(handler)
+        startTimer()
+    }
+
+    fun cancelTimer() {
+        handler = null
+        isStart = false
+        timer?.cancel()
+    }
+
+    fun timerDestroy() {
+        handler = null
+        cancelTimer()
+        timer = null
+    }
+}
+
+interface TimerHandler {
+    fun showTime(time: Long)
+    fun finishTime()
+}
